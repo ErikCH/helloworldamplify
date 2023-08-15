cc870fa4b49d2533d1332d38382bfc2dea0a4e70
Erik Hanchett <icystorm@gmail.com>
2023-08-15 09:55:44 -0700

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <h1 className="text-4xl font-bold">Hello World From Amplify Hosting</h1>
+      <h1 className="text-4xl font-bold">
+        Hello World From Amplify Hosting 123
+      </h1>
     </main>
   );
 }
